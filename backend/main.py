1627423ccb9897c491293a514e5745a5432fcfb3
Nirbhay Malhotra <nirbhaymalhotra2@gmail.com>
2024-04-28 06:10:09 +0530

    Task 4

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from pydantic import HttpUrl, BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 class VideoAnalysisRequest(BaseModel):
     youtube_link : HttpUrl
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 @app.post("/analyze_video")
 def analyze_video(request: VideoAnalysisRequest):
     from langchain_community.document_loaders import YoutubeLoader
